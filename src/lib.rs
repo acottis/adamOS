c2a1914a5f90380b70aa0ac35afc815025b2f031
Azphelumbra <azphel@nothing.com>
2020-09-06 20:54:27 +0100

    Print macro implemented and cleaned some code

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,15 @@
 
 // --------------------
 
-mod saftey;
-mod vga_buffer;
-use core::fmt::Write;
+
+mod vga_buffer; // VGA Driver
+mod saftey;     // Panic Kernel Panic Stuff
 
 // Entry point
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
 
-        
-        //vga_buffer::print_something();
-
-        write!(vga_buffer::WRITER.lock(), "test").unwrap();
-        write!(vga_buffer::WRITER.lock(), "H\n").unwrap();
-        write!(vga_buffer::WRITER.lock(), "ello \n안녕하세요").unwrap();
-        write!(vga_buffer::WRITER.lock(), "Wörld! NAOMI\n").unwrap();
-        write!(vga_buffer::WRITER.lock(), "The numbers are {} \nand {}", 42, 1.0 / 3.0).unwrap();
-
+        println!("Hello world{}", "!");
+        println!("Whats up");
         loop {}
 }
